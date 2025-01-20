fbb676bd099e62164c135b752e8143b8980877f8
M STEPHEN RAJ <stephenrajm.ug22.ad@francisxavier.ac.in>
2025-01-21 01:47:35 +0530

    Initial commit

---
+++ src/assets/frontend-assets/assets.js
@@ -0,0 +1,262 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+import img4 from './img4.png'
+import img5 from './img5.png'
+import img6 from './img6.png'
+import img7 from './img7.png'
+import img8 from './img8.png'
+import img9 from './img9.png'
+import img10 from './img10.png'
+import img11 from './img11.png'
+import img12 from './img12.png'
+import img13 from './img13.png'
+import img14 from './img14.png'
+import img15 from './img15.png'
+
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+import song9 from  './song9.mp3'
+import song10 from  './song10.mp3'
+import song11 from  './song11.mp3'
+import song12 from  './song12.mp3'
+import song13 from  './song13.mp3'
+import song14 from  './song14.mp3'
+import song15 from  './song15.mp3'
+
+
+import alb1 from './alb1.jpg'
+import alb2 from './alb2.jpg'
+import alb3 from './alb3.jpg'
+import alb4 from './alb4.jpg'
+import alb5 from './alb5.jpg'
+import alb6 from './alb6.jpg'
+import alb7 from './alb7.jpg'
+import alb8 from './alb8.jpg'
+import alb9 from './alb9.jpg'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: alb1,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: alb2,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: alb3,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: alb9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits",
+        image: alb4,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: alb8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Summer Time",
+        image: img1,
+        file:song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"4:24"
+    },
+    {
+        id:1,
+        name: "Heat Waves",
+        image: img2,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:55"
+    },
+    {
+        id:2,
+        name: "7 - Years",
+        image: img3,
+        file:song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:53"
+    },
+    {
+        id:3,
+        name: "Blinding Lights",
+        image: img4,
+        file:song4,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:23"
+    },
+    {
+        id:4,
+        name: "Stereo Hearts",
+        image: img5,
+        file:song5,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:55"
+    },
+    {
+        id:5,
+        name: "Play Date",
+        image: img14    ,
+        file:song6,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:02"
+    },
+    {
+        id:6,
+        name: "Arcade",
+        image: img7,
+        file:song7,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:05"
+    },
+    {
+        id:7,
+        name: "No Lies",
+        image: img8,
+        file:song8,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:40"
+    },
+    {
+        id:8,
+        name: "Mocking Bird",
+        image: img9,
+        file:song9,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"4:11"
+    },
+    {
+        id:9,
+        name: "Demons",
+        image: img10,
+        file:song10,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:54"
+    },
+    {
+        id:10,
+        name: "Hey Mama",
+        image: img11,
+        file:song11,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:12"
+    },
+    {
+        id:11,
+        name: "Old Town Road",
+        image: img12,
+        file:song12,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:37"
+    },
+    {
+        id:12,
+        name: "Let me Down",
+        image: img13,
+        file:song13,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:57"
+    },
+    {
+        id:13,
+        name: "Hyms of the Week",
+        image: img14,
+        file:song14,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"4:20"
+    },
+    {
+        id:14,
+        name: "Cheap Thrills",
+        image: img15,
+        file:song15,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:42"
+    }
+    
+]
